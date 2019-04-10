97fabf855ac2657e6289da9410f537f2b508eb3b
Derrick Mei <derrick.mei.168@gmail.com>
2019-04-10 05:54:52 -0500

    ReviewController

--- src/main/java/com/lambdaschool/coffeebean/controller/ReviewController.java
+++ src/main/java/com/lambdaschool/coffeebean/controller/ReviewController.java
@@ -1,27 +1,74 @@
 package com.lambdaschool.coffeebean.controller;
 
 import com.lambdaschool.coffeebean.model.Review;
-import com.lambdaschool.coffeebean.repository.Reviewrepository;
+import com.lambdaschool.coffeebean.repository.ReviewRepository;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Api(value = "Some value... by DKM", description = "Review Controller by DKM")
 @RestController
 @RequestMapping(path = "/reviews", produces = MediaType.APPLICATION_JSON_VALUE)
-public class Reviewcontroller
+public class ReviewController
 {
     @Autowired
-    Reviewrepository reviewrepos;
+    ReviewRepository reviewrepos;
 
     @GetMapping
     public List<Review> getAllReviews()
     {
         return reviewrepos.findAll();
+    }
+
+    @GetMapping("/{reviewId}")
+    public Review getReviewById(@PathVariable long reviewId)
+    {
+        return reviewrepos.findById(reviewId).get();
+    }
+
+    @PostMapping
+    public Review postNewReview(@RequestBody Review newReview)
+    {
+        return reviewrepos.save(newReview);
+    }
+
+    @PutMapping
+    public Review updateReview(@RequestBody Review updatedReview)
+    {
+        Optional<Review> foundReview = reviewrepos.findById(updatedReview.getReviewId());
+
+        if (foundReview.isPresent())
+        {
+            if (updatedReview.getHeadline() == null) updatedReview.setHeadline(foundReview.get().getHeadline());
+            if (updatedReview.getReviewBody() == null) updatedReview.setReviewBody(foundReview.get().getReviewBody());
+            if (updatedReview.getReviewedProduct() == null) updatedReview.setReviewedProduct(foundReview.get().getReviewedProduct());
+            if (updatedReview.getReviewer() == null) updatedReview.setReviewer(foundReview.get().getReviewer());
+            if (updatedReview.getStars() == null) updatedReview.setStars(foundReview.get().getStars());
+
+            return reviewrepos.save(updatedReview);
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    @DeleteMapping("/{reviewId}")
+    public Review deleteReviewById(@PathVariable long reviewId)
+    {
+        Optional<Review> foundReview = reviewrepos.findById(reviewId);
+        if (foundReview.isPresent())
+        {
+            reviewrepos.deleteById(reviewId);
+            return foundReview.get();
+        }
+        else
+        {
+            return null;
+        }
     }
 }
