38ecf6e8844d691521fcbe5ce7881e17ba22f328
Derrick Mei <derrick.mei.168@gmail.com>
2019-04-22 06:02:57 -0500

    new review model

--- src/main/java/com/lambdaschool/coffeebean/model/Review.java
+++ src/main/java/com/lambdaschool/coffeebean/model/Review.java
@@ -1,44 +1,99 @@
 package com.lambdaschool.coffeebean.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonView;
-import com.lambdaschool.coffeebean.controller.View;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
-@Data
-@NoArgsConstructor
+
 @Entity
 @Table(name = "reviews")
 public class Review
 {
-    @JsonView({View.ReviewOnly.class})
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long reviewid;
+    private long reviewId;
 
-    @JsonView({View.ReviewOnly.class})
     private String headline;
 
-    @JsonView({View.ReviewOnly.class})
-    private String reviewbody;
+    private String reviewBody;
 
-    @JsonView({View.ReviewOnly.class})
     Integer stars;
 
     // ManyToOne with User - Owner
-    @JsonView({View.ReviewOnly.class})
     @ManyToOne
-    @JoinColumn(name = "userid")
-    @JsonIgnoreProperties({"reviews", "orderhistory", "productsincart"})
+    @JoinColumn(name = "userId")
+    @JsonIgnoreProperties({
+            "reviews", "orderHistory", "cart", "currentPassword",
+            "middleName", "lastName", "customerPhone", "receiveEmails"})
     private User reviewer;
 
     // ManyToOne with Product - Owner
-    @JsonView({View.ReviewOnly.class})
     @ManyToOne
-    @JoinColumn(name = "productid")
-    @JsonIgnoreProperties({"productReviews", "productorders", "potentialusers", "suppliers"})
+    @JoinColumn(name = "productId")
+    @JsonIgnoreProperties({"productReviews", "cartItems", "suppliers"})
     private Product reviewedProduct;
+
+    public Review()
+    {
+    }
+
+    public long getReviewId()
+    {
+        return reviewId;
+    }
+
+    public void setReviewId(long reviewId)
+    {
+        this.reviewId = reviewId;
+    }
+
+    public String getHeadline()
+    {
+        return headline;
+    }
+
+    public void setHeadline(String headline)
+    {
+        this.headline = headline;
+    }
+
+    public String getReviewBody()
+    {
+        return reviewBody;
+    }
+
+    public void setReviewBody(String reviewBody)
+    {
+        this.reviewBody = reviewBody;
+    }
+
+    public Integer getStars()
+    {
+        return stars;
+    }
+
+    public void setStars(Integer stars)
+    {
+        this.stars = stars;
+    }
+
+    public User getReviewer()
+    {
+        return reviewer;
+    }
+
+    public void setReviewer(User reviewer)
+    {
+        this.reviewer = reviewer;
+    }
+
+    public Product getReviewedProduct()
+    {
+        return reviewedProduct;
+    }
+
+    public void setReviewedProduct(Product reviewedProduct)
+    {
+        this.reviewedProduct = reviewedProduct;
+    }
 }
