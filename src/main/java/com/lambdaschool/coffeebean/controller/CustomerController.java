84afed27762945e87468c4c57490082781a9fe71
Derrick Mei <derrick.mei.168@gmail.com>
2019-04-29 05:47:29 -0500

    updated: CustomerController - added security and exceptions

--- src/main/java/com/lambdaschool/coffeebean/controller/CustomerController.java
+++ src/main/java/com/lambdaschool/coffeebean/controller/CustomerController.java
@@ -1,19 +1,21 @@
 package com.lambdaschool.coffeebean.controller;
 
+import com.lambdaschool.coffeebean.exceptions.ForbiddenException;
 import com.lambdaschool.coffeebean.model.Order;
 import com.lambdaschool.coffeebean.model.User;
+import com.lambdaschool.coffeebean.repository.CartRepository;
 import com.lambdaschool.coffeebean.repository.OrderRepository;
 import com.lambdaschool.coffeebean.repository.UserRepository;
 import com.lambdaschool.coffeebean.service.CheckIsAdmin;
 import com.lambdaschool.coffeebean.service.CurrentUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
-import java.util.List;
 
 @RestController
 @RequestMapping(path = "/customer", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -24,6 +26,9 @@ public class CustomerController extends CheckIsAdmin
 
     @Autowired
     OrderRepository orderrepos;
+
+    @Autowired
+    CartRepository cartrepos;
 
     @GetMapping("/userid/{userid}")
     public Object findUserByUserid(@PathVariable long userid)
@@ -39,7 +44,7 @@ public class CustomerController extends CheckIsAdmin
 
         } else
         {
-            return doesUsernameMatch(currentUserId, userid, false);
+            throw new ForbiddenException(HttpStatus.FORBIDDEN, "user being searched does not match current user");
         }
     }
 
@@ -58,7 +63,7 @@ public class CustomerController extends CheckIsAdmin
 
             } else
             {
-                return new HashMap<String, Object>();
+                throw new ForbiddenException(HttpStatus.FORBIDDEN, "user being searched does not match current user");
             }
         }
     }
@@ -79,36 +84,53 @@ public class CustomerController extends CheckIsAdmin
 
         if (passwordEncoder.matches(unencryptedCurrentPassword, currentEncryptedPassword))
         {
-            if (updatedUser.getUsername() != null && userrepos.findByUsername(updatedUser.getUsername()) != null)
+            String updatedEmail = updatedUser.getEmail();
+            String updatedUsername = updatedUser.getUsername();
+            String updatedPhone = updatedUser.getCustomerPhone();
+            if (updatedUsername != null &&
+                    userrepos.findByUsername(updatedUsername) != null &&
+                    !updatedUsername.equalsIgnoreCase(foundUser.getUsername()))
                 returnObject.put("usernameExists", true);
-            if (updatedUser.getEmail() != null && userrepos.findByEmail(updatedUser.getEmail()) != null)
+            if (updatedEmail != null &&
+                    userrepos.findByEmail(updatedEmail) != null &&
+                    !updatedEmail.equalsIgnoreCase(foundUser.getEmail()))
                 returnObject.put("emailExists", true);
+            if (updatedPhone != null &&
+                    userrepos.findByCustomerPhone(updatedPhone) != null &&
+                    !updatedPhone.equalsIgnoreCase(foundUser.getCustomerPhone()))
+                returnObject.put("phoneExists", true);
             if (!returnObject.isEmpty()) return returnObject;
 
             // User - personal details field - can be user changed
-//            if (updatedUser.getShippingaddress() == null)
-//                updatedUser.setShippingaddress(foundUser.getShippingaddress());
-//            if (updatedUser.getBillingaddress() == null) updatedUser.setBillingaddress(foundUser.getBillingaddress());
             if (updatedUser.getCustomerPhone() == null) updatedUser.setCustomerPhone(foundUser.getCustomerPhone());
-//            if (updatedUser.getPaymentmethod() == null) updatedUser.setPaymentmethod(foundUser.getPaymentmethod());
             if (updatedUser.getMiddleName() == null) updatedUser.setMiddleName(foundUser.getMiddleName());
             if (updatedUser.getFirstName() == null) updatedUser.setFirstName(foundUser.getFirstName());
             if (updatedUser.getLastName() == null) updatedUser.setLastName(foundUser.getLastName());
             if (updatedUser.getUsername() == null) updatedUser.setUsername(foundUser.getUsername());
-            if (updatedUser.getPassword() == null) updatedUser.setPassword(foundUser.getPassword());
+            if (updatedUser.getPassword() == null) updatedUser.setPassword(updatedUser.getCurrentPassword());
             if (updatedUser.getEmail() == null) updatedUser.setEmail(foundUser.getEmail());
-            if (updatedUser.getRole() == null) updatedUser.setRole(foundUser.getRole());
+            if (updatedUser.isReceiveEmails() == true)
+            {
+                updatedUser.setReceiveEmails(true);
+            }
+            else
+            {
+                updatedUser.setReceiveEmails(false);
+            }
 
-            // fields user can't change
-            updatedUser.setUserId(currentUserId);
+            // fields user can't change or can't change at this url
+            updatedUser.setCart(cartrepos.findById(currentuser.getCartId()).get());
             updatedUser.setOrderHistory(foundUser.getOrderHistory());
+            updatedUser.setReviews(foundUser.getReviews());
+            updatedUser.setAddresses(foundUser.getAddresses());
+            updatedUser.setUserId(currentUserId);
             updatedUser.setCurrentPassword(null);
+            updatedUser.setRole("user");
 
             return userrepos.save(updatedUser);
         } else
         {
-            returnObject.put("passwordMatches", false);
-            return returnObject;
+            throw new ForbiddenException(HttpStatus.FORBIDDEN, "passwords do not match");
         }
     }
 
@@ -118,21 +140,16 @@ public class CustomerController extends CheckIsAdmin
     {
         CurrentUser currentuser = (CurrentUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         boolean isAdmin = testIsAdmin(currentuser);
-        boolean orderBelongs = false;
 
-        List<Order> userOrders = orderrepos.findAll();
+        Order foundOrder = orderrepos.findOrderByUserIdAndOrderId(currentuser.getCurrentUserId(), orderid);
 
-        for ( int i=0; i<userOrders.size(); i++)
-        {
-            if (userOrders.get(i).getOrderId() == orderid) orderBelongs = true;
-        }
-        if (orderBelongs || isAdmin)
+        if (foundOrder != null || isAdmin)
         {
             return orderrepos.findById(orderid).get();
         }
         else
         {
-            return "order does not belong to you";
+            throw new ForbiddenException(HttpStatus.FORBIDDEN, "order being searched does not belong to current user");
         }
     }
 
@@ -146,11 +163,11 @@ public class CustomerController extends CheckIsAdmin
 
         if (currentUserId == userid || isAdmin)
         {
-            return orderrepos.findAll();
+            return orderrepos.findAllUserOrdersByUserId(currentUserId);
         }
         else
         {
-            return doesUsernameMatch(currentUserId, userid, false);
+            throw new ForbiddenException(HttpStatus.FORBIDDEN, "orders being searched does not belong to current user");
         }
     }
 
